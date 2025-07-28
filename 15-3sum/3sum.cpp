2146d8e642c7cb826c83d46e879db0f34482f430
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-07-28 19:57:53 +0530

    Time: 43 ms (86.37%) | Memory: 29 MB (90.11%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int n = nums.size();
-        vector<vector<int>> result;
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i values
-            int target = -nums[i];
-            int left = i + 1;
-            int right = n - 1;
-            while (left < right) {
-                int sum = nums[left] + nums[right];
-                if (sum == target) {
-                    result.emplace_back(vector<int>{nums[i], nums[left], nums[right]}); 
-                    // Skip duplicates for left and right
-                    while (left < right && nums[left] == nums[left + 1]) ++left;
-                    while (left < right && nums[right] == nums[right - 1]) --right;
+        vector<vector<int>>re;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0; i<n-2; i++){
+            if(i>0 && nums[i]==nums[i-1]) continue; // skip duplicate
+            int target=-nums[i];
+            int left=i+1, right=n-1;
+            while(left<right){
+                int sum=nums[left]+nums[right];
+                if(sum==target){
+                    re.push_back({nums[i],nums[left],nums[right]});
+                    while(left<right && nums[left]==nums[left+1]) left++;
+                    while(left<right && nums[right]==nums[right-1]) right--;
                     left++;
                     right--;
-                } else if (sum < target) {
+                }else if(sum<target){
                     left++;
-                } else {
+                }else{
                     right--;
                 }
             }
         }
-        return result;
+        return re;
+        
     }
 };
