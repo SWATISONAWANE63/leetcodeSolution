4397a8ef3bfd1e99a73fec82cf11db7b83f20ec9
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-07-28 12:55:00 +0530

    Time: 36 ms (97.38%) | Memory: 29.7 MB (39.02%) - LeetSync

---
+++ 15-3sum/3sum.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+        void twoSum(vector<int>&nums,int k,vector<vector<int>>&result,int target){
+            int i=k, j=nums.size()-1;
+            while(i<j){
+                if(nums[i]+nums[j]>target) j--;
+                else if(nums[i]+nums[j]<target) i++;
+                else{
+                    result.push_back({-target,nums[i],nums[j]});
+                    while(i<j && nums[i]==nums[i+1]) i++;
+                    while(i<j && nums[j]==nums[j-1]) j--;
+                    i++;
+                    j--;
+                }
+            }
+        }
+        vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        if(n<3) return{};
+        vector<vector<int>>result;
+        
+        for(int i=0; i<n-2;i++){
+            if( i>0 &&nums[i]==nums[i-1]) continue;
+             int target = -nums[i];
+            twoSum(nums,i+1,result,target);
+        }
+        return result;
+    }
+};
