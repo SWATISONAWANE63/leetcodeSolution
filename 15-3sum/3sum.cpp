634f3506228c1c30dde3fb579002cd5d9bd59f06
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-07-28 13:01:07 +0530

    Time: 46 ms (73.88%) | Memory: 29.2 MB (40.24%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,33 +4,27 @@ public:
         sort(nums.begin(), nums.end());
         int n = nums.size();
         vector<vector<int>> result;
-
-        for (int i = 0; i < n - 2; ++i) {
+        for (int i = 0; i < n - 2; i++) {
             if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i values
-
             int target = -nums[i];
             int left = i + 1;
             int right = n - 1;
-
             while (left < right) {
                 int sum = nums[left] + nums[right];
                 if (sum == target) {
-                    result.emplace_back(vector<int>{nums[i], nums[left], nums[right]});
-                    
+                    result.emplace_back(vector<int>{nums[i], nums[left], nums[right]}); 
                     // Skip duplicates for left and right
                     while (left < right && nums[left] == nums[left + 1]) ++left;
                     while (left < right && nums[right] == nums[right - 1]) --right;
-
-                    ++left;
-                    --right;
+                    left++;
+                    right--;
                 } else if (sum < target) {
-                    ++left;
+                    left++;
                 } else {
-                    --right;
+                    right--;
                 }
             }
         }
-
         return result;
     }
 };
