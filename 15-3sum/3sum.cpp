1d5123aef1914e14bb79d1ed32f8c2b08e54401a
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-07-28 12:56:26 +0530

    Time: 45 ms (76.55%) | Memory: 28.9 MB (90.11%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    void twoSum(vector<int>& nums, int k, vector<vector<int>>& result, int target) {
-        int i = k, j = nums.size() - 1;
-        while (i < j) {
-            if (nums[i] + nums[j] > target) j--;
-            else if (nums[i] + nums[j] < target) i++;
-            else {
-                result.push_back({ -target, nums[i], nums[j] });
-                while (i < j && nums[i] == nums[i + 1]) i++;
-                while (i < j && nums[j] == nums[j - 1]) j--;
-                i++;
-                j--;
-            }
-        }
-    }
-
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        if (n < 3) return {};
-        
         vector<vector<int>> result;
 
-        for (int i = 0; i < n - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // Fix for duplicate skipping
+        for (int i = 0; i < n - 2; ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate i values
+
             int target = -nums[i];
-            twoSum(nums, i + 1, result, target);
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    result.emplace_back(vector<int>{nums[i], nums[left], nums[right]});
+                    
+                    // Skip duplicates for left and right
+                    while (left < right && nums[left] == nums[left + 1]) ++left;
+                    while (left < right && nums[right] == nums[right - 1]) --right;
+
+                    ++left;
+                    --right;
+                } else if (sum < target) {
+                    ++left;
+                } else {
+                    --right;
+                }
+            }
         }
+
         return result;
     }
 };
