ca850ec3a1b49a5af9e49072f957133b76b6133a
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-08-10 10:10:52 +0530

    Time: 36 ms (10.03%) | Memory: 8.9 MB (62.83%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -3,7 +3,7 @@ public:
 bool isPowerOfTwo(long long num) {
     return num > 0 && (num & (num - 1)) == 0;
 }
-    bool reorderedPowerOf2(int n) {
+bool reorderedPowerOf2(int n) {
          string s = to_string(n);
     sort(s.begin(), s.end());
 
