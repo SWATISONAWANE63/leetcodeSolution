e012c0a8540dcd56c72bb8327c104edd525281c4
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-08-10 10:09:49 +0530

    Time: 42 ms (9.14%) | Memory: 9 MB (51.03%) - LeetSync

---
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+bool isPowerOfTwo(long long num) {
+    return num > 0 && (num & (num - 1)) == 0;
+}
+    bool reorderedPowerOf2(int n) {
+         string s = to_string(n);
+    sort(s.begin(), s.end());
+
+    do {
+        if (s[0] == '0') continue; // skip leading zero
+        long long val = stoll(s);
+        if (isPowerOfTwo(val)) return true;
+    } while (next_permutation(s.begin(), s.end()));
+
+    return false;
+        
+    }
+};
