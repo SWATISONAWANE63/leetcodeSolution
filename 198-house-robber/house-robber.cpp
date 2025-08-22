d5486d6c117ab0c1ac3867a92ab3e141fe28e496
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-08-22 14:48:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (97.32%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,19 +1,17 @@
+
 class Solution {
 public:
+int t[101];
+int slove(vector<int>&nums, int i, int n){
+    if(i>=n) return 0;
+    if(t[i]!=-1) return t[i];
+    int steal=nums[i]+slove(nums,i+2,n);
+    int skip=slove(nums,i+1,n);
+    return t[i]=max(steal,skip);
+}
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0) return 0;
-        if (n == 1) return nums[0];
-
-        int prev2 = nums[0];
-        int prev1 = max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; ++i) {
-            int curr = max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-
-        return prev1;
+        int n=nums.size();
+        memset(t,-1,sizeof(t));
+        return slove (nums,0,n);
     }
 };
