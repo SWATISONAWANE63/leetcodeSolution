a7527ef2afbb063633e73e3de8869fa25a9fa627
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-08-28 12:39:43 +0530

    Time: 7 ms (73.05%) | Memory: 45 MB (40.33%) - LeetSync

---
+++ 3748-sort-matrix-by-diagonals/sort-matrix-by-diagonals.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+void sortdia(int r,int c, vector<vector<int>>&grid, bool asc, int n){
+    vector<int>vec;
+    int i=r;
+    int j=c;
+    while(i<n && j<n){
+        vec.push_back(grid[i][j]);
+        i++;
+        j++;
+    }
+    if(asc){
+        sort(begin(vec),end(vec));
+    }else{
+        sort(rbegin(vec),rend(vec));
+    }
+    i=r;
+    j=c;
+    for(int & val:vec){
+        grid[i][j]=val;
+        i++;
+        j++;
+    }
+}
+
+    vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+       int n=grid.size();
+     for(int r=0;r<n;r++){
+        sortdia(r,0,grid,false,n);
+     }
+     for(int col=1; col<n;col++){
+        sortdia(0,col,grid,true,n);
+     }
+     return grid;
+    }
+};
