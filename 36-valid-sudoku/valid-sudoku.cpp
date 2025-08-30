5219f06f8105ed346110273f296db046a93bbeb2
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-08-30 15:21:17 +0530

    Time: 0 ms (100.00%) | Memory: 24.2 MB (47.81%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,36 +1,47 @@
 class Solution {
 public:
-bool insertIfValid(unordered_set<char>& seen, char val) {
-    if (val != '.') {
-        if (seen.count(val)) return false; // duplicate found
-        seen.insert(val);
-    }
-    return true; // safe
-}
-
     bool isValidSudoku(vector<vector<char>>& board) {
-       
         int n=board.size();
+        //for checking rows
         for(int i=0; i<n;i++){
             unordered_set<char>seen;
             for(int j=0; j<n;j++){
-                if (!insertIfValid(seen, board[i][j])) return false;
-            }
-        }
-        for(int j=0; j<9; j++){
-            unordered_set<char>seen;
-            for(int i=0; i<9;i++){
-               if (!insertIfValid(seen, board[i][j])) return false;
+                char val=board[i][j];
+                if(val!='.'){
+                    if(seen.count(val)){
+                        return false;
+                    }
+                    seen.insert(val);
+                }            
 
             }
         }
+        //for checking col
+        for(int j=0; j<n; j++){
+            unordered_set<char>seen;
+            for(int i=0; i<n;i++){
+                char val =board[i][j];
+                if(val!='.'){
+                    if(seen.count(val)){
+                        return false;
+                    }
+                    seen.insert(val);
+                }
+            }
+        }
+        // for checking 3*3
         for(int rowStart=0; rowStart<9; rowStart+=3){
             for(int colStart=0; colStart<9; colStart+=3){
                 unordered_set<char>seen;
                 for(int i=rowStart; i<rowStart+3;i++){
                     for(int j=colStart; j<colStart+3; j++){
-                        if (!insertIfValid(seen, board[i][j])) return false;
-
+                        char val=board[i][j];
+                        if(val!='.'){
+                    if(seen.count(val)){
+                        return false;
+                    }
+                    seen.insert(val);
+                }
                     }
                 }
             }
