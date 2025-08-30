fa1310e1c369310fdb51631984b1d4c9b943dbbe
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-08-30 14:22:16 +0530

    Time: 7 ms (35.47%) | Memory: 24.1 MB (50.94%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
+bool insertIfValid(unordered_set<char>& seen, char val) {
+    if (val != '.') {
+        if (seen.count(val)) return false; // duplicate found
+        seen.insert(val);
+    }
+    return true; // safe
+}
+
     bool isValidSudoku(vector<vector<char>>& board) {
-        // int r=board.size();
-        // int c=board[0].size();
+       
         int n=board.size();
         for(int i=0; i<n;i++){
             unordered_set<char>seen;
             for(int j=0; j<n;j++){
-                char val=board[i][j];
-                if(val!='.'){
-                    if(seen.count(val)){
-                        return false;
-                    }
-                    seen.insert(val);
-                }            
-
+                if (!insertIfValid(seen, board[i][j])) return false;
             }
         }
         for(int j=0; j<9; j++){
             unordered_set<char>seen;
             for(int i=0; i<9;i++){
-                char val =board[i][j];
-                if(val!='.'){
-                    if(seen.count(val)){
-                        return false;
-                    }
-                    seen.insert(val);
-                }
+               if (!insertIfValid(seen, board[i][j])) return false;
+
             }
         }
         for(int rowStart=0; rowStart<9; rowStart+=3){
@@ -34,13 +29,8 @@ public:
                 unordered_set<char>seen;
                 for(int i=rowStart; i<rowStart+3;i++){
                     for(int j=colStart; j<colStart+3; j++){
-                        char val=board[i][j];
-                        if(val!='.'){
-                    if(seen.count(val)){
-                        return false;
-                    }
-                    seen.insert(val);
-                }
+                        if (!insertIfValid(seen, board[i][j])) return false;
+
                     }
                 }
             }
