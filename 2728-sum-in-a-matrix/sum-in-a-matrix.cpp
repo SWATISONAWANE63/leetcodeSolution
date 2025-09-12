431ad1306385df4c238c4d97f9ec28f93b190079
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-09-13 00:16:46 +0530

    Time: 15 ms (70.57%) | Memory: 53.1 MB (80.38%) - LeetSync

--- 2728-sum-in-a-matrix/sum-in-a-matrix.cpp
+++ 2728-sum-in-a-matrix/sum-in-a-matrix.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int matrixSum(vector<vector<int>>& nums) {
-        int m = nums.size();
-        int n = nums[0].size();
-
-        // Step 1: Sort each row
-        for (int i = 0; i < m; ++i) {
+        int n=nums.size();
+        int m=nums[0].size();
+        for(int i=0; i<n;i++){
             sort(nums[i].begin(), nums[i].end());
         }
-
-        int score = 0;
-
-        // Step 2: For each column, take the max across all rows
-        for (int j = 0; j < n; ++j) {
-            int maxVal = 0;
-            for (int i = 0; i < m; ++i) {
-                maxVal = max(maxVal, nums[i][j]);
+        int finalSum=0;
+        for(int j=0; j<m; j++){
+             int tempSum=0;
+            for(int i=0; i<n;i++){
+                tempSum=max(tempSum,nums[i][j]);
+               
             }
-            score += maxVal;
+            finalSum+=tempSum;
         }
-
-        return score;
+       return finalSum;
+        
     }
 };
