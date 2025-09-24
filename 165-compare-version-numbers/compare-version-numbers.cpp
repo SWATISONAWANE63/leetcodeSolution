0eb197269a8158e926ef9fe4b2418661dfaaf968
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-09-24 11:02:17 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (16.02%) - LeetSync

--- 165-compare-version-numbers/compare-version-numbers.cpp
+++ 165-compare-version-numbers/compare-version-numbers.cpp
@@ -2,24 +2,22 @@ class Solution {
 public:
     int compareVersion(string version1, string version2) {
         vector<int>v1,v2;
-        stringstream he1(version1);
-        string num;
-        while(getline(he1,num,'.')){
-            v1.push_back(stoi(num));
+       string part;
+        stringstream ss1(version1);
+        while(getline(ss1,part,'.')){
+               v1.push_back(stoi(part));
         }
-        stringstream he2(version2);
-        while(getline(he2,num,'.')){
-            v2.push_back(stoi(num));
+        stringstream ss2(version2);
+        while(getline(ss2,part,'.')){
+               v2.push_back(stoi(part));
         }
-        int n=max(v1.size(),v2.size());
-        for(int i=0; i<n;i++){
-            int a=(i<v1.size())? v1[i]:0;
-            int b=(i<v2.size())? v2[i]:0;
-            if(a>b) return 1;
-            if(a<b) return -1;
-            
-
-        }
-        return 0;
+       int n = max(v1.size(), v2.size());
+for (int i = 0; i < n; i++) {
+    int num1 = (i < v1.size()) ? v1[i] : 0;
+    int num2 = (i < v2.size()) ? v2[i] : 0;
+    if (num1 > num2) return 1;
+    if (num1 < num2) return -1;
+}
+return 0;
     }
 };
