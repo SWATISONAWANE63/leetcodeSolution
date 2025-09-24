68f7a7db861a1a431126ccaee359105cbf89ec37
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-09-25 00:06:14 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (95.07%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        unordered_map<char,int>count;
-        for(char c:s){
-            count[c]++;
+        int sum_s=0;
+        int sum_t=0;
+        for(char ch:s){
+            sum_s+=ch;
         }
-        for(char c:t){
-            count[c]--;
-            if(count[c]<0){
-                return c;
-            }
+        for(char ch:t){
+            sum_t+=ch;
         }
-        return 'x';
+        return (char)(sum_t-sum_s);
     }
 };
