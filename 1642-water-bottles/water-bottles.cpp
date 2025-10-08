a9ef7c5fb82f20164fabb2e379e59241e641bb97
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-10-08 13:54:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (79.95%) - LeetSync

--- 1642-water-bottles/water-bottles.cpp
+++ 1642-water-bottles/water-bottles.cpp
@@ -1,6 +1,12 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        return numBottles+(numBottles-1)/(numExchange-1);
+        int total=0;
+        while(numBottles>=numExchange){
+          total+=numExchange;
+           numBottles-=numExchange;
+           numBottles+=1;
+        }
+        return total+numBottles;
     }
 };
