fc40e0031fc3bea14660866023d9b9004738b1c8
swati sonawane <160817801+SWATISONAWANE63@users.noreply.github.com>
2025-10-10 13:11:26 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (68.61%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string prefix=strs[0];
-        for(int i=1; i<strs.size();i++){
-            while(strs[i].find(prefix)!=0){
-                prefix=prefix.substr(0,prefix.size()-1);
-                if(prefix.empty())return "";
-            }
+        int n=strs.size();
+        string ans="";
+        for(int i=0; i<strs[0].size();i++){
+            char ch=strs[0][i];
+            for(int j=1; j<n;j++){
+            if(i>=strs[j].size()||strs[j][i]!=ch){
+                return ans;
+            }          
         }
-        return prefix;
+         ans+=ch;
+        }
+        return ans;
     }
 };
